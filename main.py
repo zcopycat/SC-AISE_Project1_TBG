1d175b6d7028eecc89a2f4ac2fcc5e183796aa8d
zcopycat <zvilsdorfa@outlook.com>
2025-01-20 23:21:32 +0000

    Set room names

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from room import Room
 
-kitchen = Room("kitchen")
-ballroom = Room("ballroom")
-dining_hall = Room ("dining hall")
-
+kitchen = Room()
+kitchen.set_name("kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies")
-kitchen.describe()
 
-ballroom.set_description("An big oval shaped room decorated with baroque style golden ornaments and mirrors")
-ballroom.describe()
+ballroom = Room()
+ballroom.set_name("ballroom")
+ballroom.set_description("A big oval shaped room decorated with baroque style golden ornamentation and mirrors")
 
+dining_hall = Room()
+dining_hall.set_name("dining hall")
 dining_hall.set_description("A candle-lit room with stone floors, wooden furniture, and a fire place")
-dining_hall.describe()
 
 kitchen.link_room(dining_hall, "south")
 dining_hall.link_room(kitchen, "north")
 dining_hall.link_room(ballroom,"west")
 ballroom.link_room(dining_hall,"east")
+
+kitchen.get_details()
