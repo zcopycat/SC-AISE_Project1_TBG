6b7fb598e67f7554b260ca8bf75ebb96fb556a4e
zcopycat <zvilsdorfa@outlook.com>
2025-01-23 18:49:29 +0000

    Import Enemy (Character) and update while True statement

--- main.py
+++ main.py
@@ -18,9 +18,24 @@ dining_hall.link_room(ballroom,"west")
 ballroom.link_room(dining_hall,"east")
 
 current_room = kitchen
+
 while True:
     print("\n")
     current_room.get_details()
-    command = input("> ")
-    current_room = current_room.move(command)
-    break
+
+    inhabitant = current_room.get_character()
+    if inhabitant is not None:
+        inhabitant.describe()
+
+        command = input("> ")
+        current_room = current_room.move(command)
+        break
+
+from character import Enemy # If removeing the break in the above block of code, Code is unreacable Pylance - 
+
+dave = Enemy("Dave", "A smelly zombie")
+dave.set_conversation("Brrlgrhaargh... rghlaar... brains...")
+dave.set_weakness("beer")
+dining_hall.set_character(dave)
+
+# Code is unreachable (Exception?) error - If you're calling a function that we know is going to never return, it means the code after that can't be analysed.
