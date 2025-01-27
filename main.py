af03d156b68315b88f796a1b5d6db99434353e34
zcopycat <zvilsdorfa@outlook.com>
2025-01-27 18:33:43 +0000

    Comment out fight method outside while function and add 'talk' and 'fight' instructions in the inabitant.describe str

--- main.py
+++ main.py
@@ -5,24 +5,21 @@ from character import Enemy
 # Add a truth condition to ...?
 dead = False
 
-# Setting up the rooms  
-kitchen = Room()
-kitchen.set_name("kitchen")
+# Setting up the rooms
+kitchen = Room("Kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies")
 
-ballroom = Room()
-ballroom.set_name("ballroom")
+ballroom = Room("Ballroom")
 ballroom.set_description("A big oval shaped room decorated with baroque style golden ornamentation and mirrors")
 
-dining_hall = Room()
-dining_hall.set_name("dining hall")
+dining_hall = Room("Dining hall")
 dining_hall.set_description("A candle-lit room with stone floors, wooden furniture, and a fire place")
 
 # Linking rooms to each other 
 kitchen.link_room(dining_hall, "south")
 dining_hall.link_room(kitchen, "north")
-dining_hall.link_room(ballroom,"west")
-ballroom.link_room(dining_hall,"east")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
 
 # Creating the instance of good old Dave 
 dave = Enemy("Dave", "A smelly zombie")
@@ -35,19 +32,20 @@ dave.set_conversation("Hi my name is Dave. How about I eat your brain?")
 dave.set_weakness("beer")
 dave.set_inventory("beer")
 dave.steal("beer")
-print("What will fight with?")
-fight_with = input("\n> ")
-dave.fight(fight_with)
+# #print("What will you fight with?")
+# fight_with = input("\n> ")
+# dave.fight(fight_with)
 
 # Create current room plus interaction with the character in the room
-current_room = kitchen
+current_room = dining_hall
 while dead == False:
     print("\n")
     current_room.get_details()
     inhabitant = current_room.get_character()
     if inhabitant is not None:
         inhabitant.describe()
-    print("Which direction would you like to go? North, east, south, west")
+    # Added 'talk' and 'fight' instructions to the string so that these appear in the console for the player to read
+    print("Which direction would you like to go? North, east, south, west \n Or would you rather 'talk' or 'fight'?")
     command = input("\n> ")
     if command in ["north", "south", "east", "west"]:
         current_room = current_room.move(command)
