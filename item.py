b89fc7e24b25f9b5e3820285764950e6861fa11d
zcopycat <zvilsdorfa@outlook.com>
2025-01-27 00:50:08 +0000

    Add describe_item method

--- item.py
+++ item.py
@@ -2,7 +2,8 @@ class Item:
     def __init__(self):
         self.name = None
         self.description = None
-        self.quantity = None
+        # self.value = None
+        # self.quantity = None
 
     def get_name(self):
         return self.name
@@ -16,8 +17,17 @@ class Item:
     def set_description(self, item_description):
         self.description = item_description
 
-    def get_quantity(self):
-        return self.quantity
+    def describe_item(self):
+        print(self.description)
+
+    # def get_item_value(self):
+    #     return self.value
     
-    def set_quantity(self, item_quantity):
-        self.quantity = item_quantity
+    # def set_item_value(self, item_value):
+    #     self.value = item_value
+
+    # def get_quantity(self):
+    #     return self.quantity
+    
+    # def set_quantity(self, item_quantity):
+    #     self.quantity = item_quantity
